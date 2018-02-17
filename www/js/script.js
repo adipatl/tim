ace159dea0ecdc80b5ff8561cc51f49fc2eaa880
Adipat Larprattanakul <adipat.larprattanakul@thomsonreuters.com>
2018-02-18 00:48:13 +0700

    use cache instead

--- www/js/script.js
+++ www/js/script.js
@@ -26,7 +26,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        checkCookie();
+        checkCache();
     }
 };
 
@@ -37,7 +37,7 @@ function redirect() {
 
   const employeeId = document.getElementById("employeeId").value;
   const email = document.getElementById("email").value;
-  createCookie("employeeId", employeeId, 30);
+  getFromCache("employeeId", employeeId, 30);
   var redirect = redirectTo(email, employeeId);
 }
 
@@ -106,34 +106,17 @@ function checkValid() {
   document.getElementById("button").disabled = employeeId.length <=0 || email.length <= 0;
 }
 
-function checkCookie() {
-  document.getElementById("employeeId").value = readCookie("employeeId");
+function checkCache() {
+  document.getElementById("employeeId").value = readFromCache("employeeId");
 
 }
 
-// cookies utils
-// https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
-function createCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + value + expires + "; path=/";
+function getFromCache(name, value) {
+    var storage = window.localStorage;
+    storage.setItem(name, value); // Pass a key name and its value to add or update that key.
 }
 
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
-function eraseCookie(name) {
-  createCookie(name, "", -1);
+function readFromCache(name) {
+    var storage = window.localStorage;
+    return storage.getItem(name); // Pass a key name and its value to add or update that key.
 }
